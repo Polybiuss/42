19a0b3a98e2c1511295e1a7564dd6a61026e5256
Suiten <johnb16@hotmail.fr>
2024-08-03 22:13:10 +0200

    ex00

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,13 @@
+void    ft_ft(int *nbr)
+{
+    *nbr = 42;
+}
+/*
+#include <stdio.h>
+int main(void)
+{
+    int nbr = 12;
+    printf("%d\n", nbr);
+    ft_ft(&nbr);
+    printf("%d\n", nbr);
+}*/
