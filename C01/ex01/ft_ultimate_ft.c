fb1f9c3c6b9830826d4b259c4bc11f526d5aa3ec
Suiten <johnb16@hotmail.fr>
2024-08-03 22:23:34 +0200

    ex01

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,34 @@
+void    ft_ultimate_ft(int *********nbr)
+{
+    *********nbr = 42;
+}
+/*
+#include <stdio.h>
+int main(void)
+{
+    int a = 10;
+    int *b;
+    int **c;
+    int ***d;
+    int ****e;
+    int *****f;
+    int ******g;
+    int *******h;
+    int ********i;
+    int *********j;
+
+    b = &a;
+    c = &b;
+    d = &c;
+    e = &d;
+    f = &e;
+    g = &f;
+    h = &g;
+    i = &h;
+    j = &i;
+
+    printf("%d\n", *********j);
+    ft_ultimate_ft(j);
+    printf("%d\n", *********j);
+    return (0); 
+}*/
